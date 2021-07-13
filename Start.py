02e9db40c7fb39d5a9a622ec00f8a4e6a9b58e15
iolan <yvivesgilabert@gmail.com>
2021-07-13 09:57:44 +0200

    Start

---
+++ Start.py
@@ -0,0 +1,27 @@
+"""
+Startup Code
+
+
+"""
+
+import sys
+sys.path.append('../marcos_client')
+from PyQt5.QtWidgets import QApplication
+from controller.mainviewcontroller import MainViewController
+import cgitb 
+cgitb.enable(format = 'text')
+
+
+VERSION = "0.1.0"
+AUTHOR = "Yolanda Vives"
+
+if __name__ == '__main__':
+    print("Processing Toolbox for Magnetic Resonance Imaging")
+    
+   
+    app = QApplication(sys.argv)
+    gui = MainViewController()
+    gui.show()
+    sys.exit(app.exec_())
+
+        
